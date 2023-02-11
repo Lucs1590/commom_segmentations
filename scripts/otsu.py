a6dc4a1004867db348c0052d8580eaf117ac5036
Lucas Brito <lucasbsilva29@gmail.com>
2023-02-11 13:06:14 -0300

    feat: applying gausian before otsu limiarization

--- scripts/otsu.py
+++ scripts/otsu.py
@@ -1,17 +1,18 @@
-from matplotlib import pyplot as plt
 import os
-import cv2
 import logging
+from typing import Union
 
+import cv2
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg', force=True)
+from matplotlib import pyplot as plt
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def apply_otsu(img: np.ndarray) -> np.ndarray:
+def apply_otsu(img: np.ndarray, convert_to_gray: bool = True) -> Union[np.ndarray, int]:
     """ Binarize the image using Otsu's method.
 
     Args:
@@ -24,8 +25,10 @@ def apply_otsu(img: np.ndarray) -> np.ndarray:
         return None
 
     logger.info('Applying Otsu\'s method')
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    if convert_to_gray:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 
 if __name__ == '__main__':
@@ -54,3 +57,9 @@ if __name__ == '__main__':
     plt.savefig(os.path.join(results_dir, 'histogram2.png'))
     plt.clf()
 
+    gausian = cv2.GaussianBlur(gray, (5, 5), 0)
+    cv2.imwrite(os.path.join(results_dir, 'gausian.png'), gausian)
+
+    threshold, binarized_img = apply_otsu(gausian, convert_to_gray=False)
+    logger.info(f'Threshold: {threshold}')
+    cv2.imwrite(os.path.join(results_dir, 'otsu_gausian.png'), binarized_img)
