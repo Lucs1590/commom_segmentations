19ede1c7c85d9d05c93a920b707b727cbc12e316
Lucas Brito <lucasbsilva29@gmail.com>
2023-02-11 11:54:24 -0300

    feat(binarization): add typing and color conversion

--- scripts/binarization.py
+++ scripts/binarization.py
@@ -1,8 +1,9 @@
 import os
 import cv2
+import numpy as np
 
 
-def apply_commom_binarize(img, threshold=127):
+def apply_commom_binarize(img: np.ndarray, threshold: int) -> np.ndarray:
     """Binarize the image using the given threshold.
 
     Args:
@@ -12,20 +13,20 @@ def apply_commom_binarize(img, threshold=127):
     Returns:
         numpy.ndarray: The binarized image.
     """
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     return cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)[1]
 
 
 if __name__ == '__main__':
     image_path = os.path.join('images', 'paisagem01.jpg')
-    output_path = os.path.join('results', 'binarized.png')
-    threshold = 127
+    threshold = 140
 
     os.makedirs('results', exist_ok=True)
 
-    img = cv2.imread(
-        image_path,
-        cv2.IMREAD_GRAYSCALE
-    )
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    cv2.imwrite(img, 'results/original.png')
 
     binarized = apply_commom_binarize(img, threshold)
-    cv2.imwrite(output_path, binarized)
+    cv2.imwrite(binarized, 'results/binarized.png')
