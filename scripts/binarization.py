99882ed8acb7159421c4ed24cbd90ebbcb161cb5
Lucas Brito <lucasbsilva29@gmail.com>
2023-02-11 12:04:33 -0300

    feat: add more than one binarization type

--- scripts/binarization.py
+++ scripts/binarization.py
@@ -1,23 +1,32 @@
 import os
 import cv2
+import logging
+
 import numpy as np
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def apply_commom_binarize(img: np.ndarray, threshold: int) -> np.ndarray:
+
+def apply_binarization(img: np.ndarray, threshold: int, threshold_type: int = cv2.THRESH_BINARY) -> np.ndarray:
     """Binarize the image using the given threshold.
 
     Args:
         img (numpy.ndarray): The image to be binarized.
         threshold (int): The threshold to be used.
+        threshold_type (int, optional): The threshold type. Defaults to cv2.THRESH_BINARY.
 
     Returns:
         numpy.ndarray: The binarized image.
     """
+    logger.info(f'Applying binarization with threshold {threshold} and threshold type {threshold_type}')
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    return cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)[1]
+    return cv2.threshold(img, threshold, 255, threshold_type)[1]
 
 
 if __name__ == '__main__':
+    threshold_types = [cv2.THRESH_BINARY, cv2.THRESH_BINARY_INV,
+                       cv2.THRESH_TRUNC, cv2.THRESH_TOZERO, cv2.THRESH_TOZERO_INV]
     image_path = os.path.join('images', 'paisagem01.jpg')
     threshold = 140
 
@@ -26,7 +35,14 @@ if __name__ == '__main__':
     img = cv2.imread(image_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    cv2.imwrite(img, 'results/original.png')
+    cv2.imwrite('results/original.png', img)
 
-    binarized = apply_commom_binarize(img, threshold)
-    cv2.imwrite(binarized, 'results/binarized.png')
+    for threshold_type in threshold_types:
+        cv2.imwrite(
+            f'results/binarization_{threshold_type}.png',
+            apply_binarization(
+                img,
+                threshold,
+                threshold_type
+            )
+        )
